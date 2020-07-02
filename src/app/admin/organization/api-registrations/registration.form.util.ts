19a0c6d0309c2e631d244f3e327c3dc81bad3fdf
Ruben <rubenski@gmail.com>
2020-07-02 11:34:32 +0200

    Don't show error message if form array is not dirty

--- src/app/admin/organization/api-registrations/registration.form.util.ts
+++ src/app/admin/organization/api-registrations/registration.form.util.ts
@@ -53,7 +53,7 @@ export class ApiRegistrationFormUtil {
         // sum the number of checked checkboxes
         .reduce((prev, next) => next ? prev + next : prev, 0);
       // if the total is not greater than the minimum, return the error message
-      return totalSelected >= min ? null : {required: true};
+      return !formArray.dirty || totalSelected >= min ? null : {required: true};
     };
 
     return validator;
